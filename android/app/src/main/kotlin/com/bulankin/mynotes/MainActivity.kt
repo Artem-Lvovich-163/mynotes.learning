a6c398e3810ee0fd70f6a0bd73d2c15126c07c80
artem <artem.bulankin@gmail.com>
2023-02-01 08:44:16 +0400

    hello

---
+++ android/app/src/main/kotlin/com/bulankin/mynotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bulankin.mynotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mynotes/views/login_view.dart';
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const LoginView(),
+    );
+  }
+}
+
+// РЕГИСТРАЦИЯ
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Регистрация'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Column(
+                children: [
+                  TextField(
+                    controller: _email,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration:
+                        const InputDecoration(hintText: 'Введите свою почту'),
+                  ),
+                  TextField(
+                    controller: _password,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    decoration:
+                        const InputDecoration(hintText: 'Введите пароль'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      final email = _email.text;
+
+                      final password = _password.text;
+                      try {
+                        final userCredential = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                          email: email,
+                          password: password,
+                        );
+
+                        print(userCredential);
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'weak-password') {
+                          print('weak-password');
+                        } else if (e.code == 'email-already-in-use') {
+                          print('email already in use');
+                        } else if (e.code == 'invalid-email') {
+                          print(e.code);
+                        }
+                      }
+                    },
+                    child: const Text('Регистрация'),
+                  ),
+                ],
+              );
+            default:
+              return const Text('Загрузка...');
+          }
+        },
+      ),
+    );
+  }
+}
